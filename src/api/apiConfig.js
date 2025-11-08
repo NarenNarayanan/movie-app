00c906d0716955a4ac5ac1206306e9612a04274b
NarenNarayanan <narenn4406@gmail.com>
2025-11-08 11:21:33 +0530

    configure backend for deploy

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,8 +1,8 @@
 const apiConfig = {
   // Redirect all API calls through your backend proxy
-  baseUrl: "http://localhost:5000/api/tmdb",  // Remove trailing slash
+  //baseUrl: "http://localhost:5000/api/tmdb",  // Remove trailing slash
   //WHEN DEPLOYING :
-  //baseUrl: "https://your-backend-service.onrender.com/api/tmdb/"
+  baseUrl: "https://your-backend-service.onrender.com/api/tmdb/",
   
   originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
   w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`,
